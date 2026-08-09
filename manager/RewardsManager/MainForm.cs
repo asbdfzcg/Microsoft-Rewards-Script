93a713f2f176ced7a06b0647218f1fe48bb1f9ac
asdfzg <3261264108@qq.com>
2026-08-09 11:10:11 +0800

    refactor(manager): revert update tab to 4-button left-aligned row
    - Restore FlowLayoutPanel horizontal layout with all four buttons left-aligned
    - Buttons: 立即更新 / 跳过此版本 / 重新检查更新 / 项目主页
    - Rebuild autorun/RewardsManager.exe

--- manager/RewardsManager/MainForm.cs
+++ manager/RewardsManager/MainForm.cs
@@ -1260,39 +1260,17 @@ namespace RewardsManager
             };
             grpLog.Controls.Add(txtChangelog);
 
-            var btnRow = new TableLayoutPanel
+            var btnRow = new FlowLayoutPanel
             {
                 Dock = DockStyle.Bottom,
                 AutoSize = true,
-                ColumnCount = 3,
-                RowCount = 1,
-                Margin = new Padding(0, 0, 0, 8)
-            };
-            btnRow.ColumnStyles.Add(new ColumnStyle(SizeType.AutoSize));
-            btnRow.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100f));
-            btnRow.ColumnStyles.Add(new ColumnStyle(SizeType.AutoSize));
-
-            var leftFlow = new FlowLayoutPanel
-            {
-                AutoSize = true,
                 FlowDirection = FlowDirection.LeftToRight,
                 WrapContents = true,
-                Margin = Padding.Empty
+                Margin = new Padding(0, 0, 0, 8)
             };
             btnUpdate = MkButton("立即更新", async (_, _) => await DoUpdate());
             btnSkip = MkButton("跳过此版本", (_, _) => SkipVersion());
             var btnRecheck = MkButton("重新检查更新", (_, _) => RecheckUpdates());
-            leftFlow.Controls.Add(btnUpdate);
-            leftFlow.Controls.Add(btnSkip);
-            leftFlow.Controls.Add(btnRecheck);
-
-            var rightFlow = new FlowLayoutPanel
-            {
-                AutoSize = true,
-                FlowDirection = FlowDirection.LeftToRight,
-                WrapContents = false,
-                Margin = Padding.Empty
-            };
             var btnHome = MkButton("项目主页", (_, _) =>
             {
                 System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
@@ -1301,10 +1279,10 @@ namespace RewardsManager
                     UseShellExecute = true
                 });
             });
-            rightFlow.Controls.Add(btnHome);
-
-            btnRow.Controls.Add(leftFlow, 0, 0);
-            btnRow.Controls.Add(rightFlow, 2, 0);
+            btnRow.Controls.Add(btnUpdate);
+            btnRow.Controls.Add(btnSkip);
+            btnRow.Controls.Add(btnRecheck);
+            btnRow.Controls.Add(btnHome);
 
             panel.Controls.Add(grpLog);
             panel.Controls.Add(btnRow);
